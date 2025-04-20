8e7c12fc249f1a9f6899b6310ceeb8e47b5626a6
AbuBakr-Omonov <omonov0780@gmail.com>
2025-04-20 17:06:28 +0500

    DOM Manipulating

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const mainImageEl = document.querySelector(".iphone__image img")
+
+function changeImage(path){
+    mainImageEl.setAttribute("src", path)
+}
+
+
+const cardEL = document.querySelector(".card")
+console.log(cardEL);
+
